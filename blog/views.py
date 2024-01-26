03e0f3b127a83067de76c76ca602fa0808da2430
sandro-silva-de-paula <sandrosilvadepaula.usa@gmail.com>
2024-01-25 20:11:35 -0500

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 
 def my_view_blog(request):
-    return HttpResponse('Uma mensagem para alguem especial em Blog do blog app_1')
+    return render(request,'blog/index.html')
 
 def my_view_exemplo(request):
-    return HttpResponse('Uma mensagem para alguem especial em Blog do blog  exemplo app_1')
+    return render(request,'blog/exemplo.html')
--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def my_view_home(request):
-    return HttpResponse('Uma mensagem para alguem especial em Home em home app_1')
+    return render(request,'home/index.html')
