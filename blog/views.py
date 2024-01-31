b066afe9144bccd4844fc430988132bbc24729c0
sandro-silva-de-paula <sandrosilvadepaula.usa@gmail.com>
2024-01-30 21:07:12 -0500

    busacando dados num dic/lista/jsom

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render
+from blog.data import posts
 
 # Create your views here.
 
 
 def my_view_blog(request):
     contexto = {
-        'text': 'texto como context parametro....... index',
-        'text_2': 'ola blog',
-        'title': 'BLog inicial - ',
-        'text_part': 'testando partial head - vindo  blog',
-        'text_parag': 'paragrafo.............. do blog'
+        'text': 'texto como context parametro....... index blog',
+        'title': 'Blog Inicial - ',
+        'posts': posts
         }
     return render(request,'blog/index.html', contexto)
 
 def my_view_exemplo(request):
     contexto = {
         'text': 'texto como context parametro.......exemplo',
-        'text_2': 'ola exemplo',
-        'title': 'Blog_exemplo - ',
-        'text_part': 'testando partial head - vindo do exemplo',
-        'text_parag': 'paragrafo.............. Exemplo'
-                }
+        'title': 'Exemplo Inicial - ',
+        'posts': posts
+             }
     return render(request,'blog/exemplo.html', contexto)
--- home/views.py
+++ home/views.py
@@ -3,10 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def my_view_home(request):
     contexto = {
-        'text': 'texto como context parametro.......',
-        'text_2': 'ola home',
-         'title': 'Home - ',
-         'text_part': 'testando partial head -  vindo do home',
-         'text_parag': 'paragrafo..............'
+        'text': 'texto como context parametro....... HOME',
+        'title': 'Pagina Inicial - ',
+
         }
     return render(request,'home/index.html', contexto)
