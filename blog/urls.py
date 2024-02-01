a1782bc6d5d25cbf34276c33c184a9b814f7e925
sandro-silva-de-paula <sandrosilvadepaula.usa@gmail.com>
2024-01-31 22:45:24 -0500

    penultima aula, urls dinamicas, um pouco de css

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,10 @@
 from django.urls import path
 from blog import views 
 
-
+app_name = 'blog'
 urlpatterns = [
     path('', views.my_view_blog,name='blog'),
+    path('post/<int:_id>', views.my_view_post,name='post'),
     path('exemplo/', views.my_view_exemplo, name='exemplo'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from blog.data import posts
+from typing import Any
+from django.http import HttpRequest, Http404 
 
 # Create your views here.
 
@@ -19,3 +21,20 @@ def my_view_exemplo(request):
         'posts': posts
              }
     return render(request,'blog/exemplo.html', contexto)
+
+def my_view_post(request:HttpRequest,_id: int):
+    found_post : dict[str,Any] | None = None
+    for post in posts:
+        if post['id'] == _id:
+           found_post=post
+           break 
+    # pra nao ficar dando erro de tipo em found_post    
+    if found_post is None:
+        raise Http404('Post inesixtente')    
+
+    contexto = {
+        'text': 'texto como context parametro.......post',
+        'title': found_post['title'] +'- ',
+        'post': found_post
+                                  }
+    return render(request,'blog/post.html', contexto)
